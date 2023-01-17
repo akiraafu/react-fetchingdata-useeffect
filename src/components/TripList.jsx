6ec684dfeca54cb49ffa9acb291bc37535c45850
Junrong Fu <junrongfu6@gmail.com>
2023-01-17 20:51:08 +0800

    first commit

---
+++ src/components/TripList.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
+
+//styles
+import "./tripList.css";
+
+const TripList = () => {
+  // const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
+
+  const { data: trips, isPending } = useFetch(url);
+
+  // const fetchTrips = useCallback(async () => {
+  //   const response = await fetch(url);
+  //   const json = await response.json();
+  //   setTrips(json);
+  // }, [url]);
+
+  // useEffect(() => {
+  //   // fetch(url)
+  //   //   .then((response) => response.json())
+  //   //   .then((json) => setTrips(json));
+  //   fetchTrips();
+  // }, [fetchTrips]);
+  console.log(trips);
+
+  return (
+    <div className="trip-list">
+      <h2>Trip List</h2>
+      {isPending && <div>Loading trips...</div>}
+      <ul>
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p> {trip.price}</p>
+            </li>
+          ))}
+      </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All trips
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TripList;
